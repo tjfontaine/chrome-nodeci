3a3217990ee0166f9e59f554cec235808b1dc856
Timothy J Fontaine <tjfontaine@gmail.com>
2013-03-19 11:43:16 -0700

    refactor to only get storage once

--- github_pull.js
+++ github_pull.js
@@ -1,5 +1,12 @@
 var HOSTNAME = 'jenkins.nodejs.org';
-var URL = 'http://' + HOSTNAME + window.location.pathname;
+var STORAGE = [
+  'JENKINS_USERNAME',
+  'JENKINS_API_TOKEN',
+];
+
+function canBuild(opts) {
+  return opts.JENKINS_USERNAME && opts.JENKINS_API_TOKEN;
+}
 
 function getSI(data, klass) {
   var state = 'class="'+klass+' state-indicator ';
@@ -20,74 +27,85 @@ function getSI(data, klass) {
   return state;
 };
 
-function buildPR(pr) {
-  chrome.storage.sync.get(['JENKINS_USERNAME', 'JENKINS_API_TOKEN'], function (opts) {
-    if (opts.JENKINS_USERNAME && opts.JENKINS_API_TOKEN)
-      $.post('http://' + HOSTNAME + pr + '?' + $.param(opts), function () { location.reload(); });
-  });
+function buildPR(elm, opts) {
+  if (canBuild(opts)) {
+    var pr = $(elm).data('pr');
+    $.post('http://' + HOSTNAME + pr + '?' + $.param(opts), function () { location.reload(); });
+  }
 }
 
-$('.pulls-list div').each(function (i, div) {
-  var h3 = $(div).children('h3');
-  if (h3.length) {
-    var url = 'http://' + HOSTNAME;
-    var a = $(div).children('h3').children('a')[0];
-    url += a.pathname;
-    $.getJSON(url, function (data) {
-      $(a).after('<button data-pr="'+ a.pathname +'" ' + getSI(data, 'jenkins-build') +'>'+ data.status + '</div>');
-      $('.jenkins-build').click(function () {
-        buildPR($(this).data('pr'));
-      });
-    });
-  }
-});
-
-if (URL.match(/\d+$/)) {
-$.getJSON(URL, function (data) {
-  $('#pr_contributors_box').before('<div id="jenkins"></div>');
-  $('#jenkins').append('<div id="jenkinsBuildIndicator" ' + getSI(data, '') + '>'+ data.status + '</div>');
-  $('#jenkins').append('<div id="jenkinsBuildResults" class="discussion-bubble-inner"></div>');
-
-  if (data.status != 'PASSING') {
-    var html = '<table class="commits commits-conendsed" width="100%">';
-    data.result = data.result || {};
-    $.each(data.result, function (i) {
-      var result = data.result[i];
-      if (!result) return;
-      if (result.length) {
-        result.forEach(function (test) {
-          html += '<tr class="commit js-details-container js-socket-channel js-updatable-content">';
-          html += '<td class="author">' + i + '</td>';
-          html += '<td class="message"><code>';
-          html += '<a target="_new" href="' + test.url.replace('/artifact/test.tap', '');
-          html += '/tapTestReport/test.tap-' + test.number + '/">';
-          html += test.desc + '</a></code></td>';
-          html += '</tr>';
+function pullList(opts) {
+  $('.pulls-list div').each(function (i, div) {
+    var h3 = $(div).children('h3');
+    if (h3.length) {
+      var url = 'http://' + HOSTNAME;
+      var a = $(div).children('h3').children('a')[0];
+      url += a.pathname;
+      $.getJSON(url, function (data) {
+        $(a).after('<button data-pr="'+ a.pathname +'" ' + getSI(data, 'jenkins-build') +'>'+ data.status + '</div>');
+        $('.jenkins-build').click(function () {
+          buildPR(this, opts);
         });
-      }
-    });
-    html += '</table>';
-    $('#jenkinsBuildResults').append(html);
-  }
-
-  if (data.url)
-    $('#jenkinsBuildResults').append('<a target="_new" href="' + data.url + '">Last build</a>');
-
-  //if (data.started)
-  //  $('#jenkinsBuildResults').append(' Started at: ' + data.started);
-
-  if (data.by)
-    $('#jenkinsBuildResults').append(' By: ' + data.by);
-
-  if (data.status == 'BUILDING') return;
-
-  chrome.storage.sync.get(['JENKINS_USERNAME', 'JENKINS_API_TOKEN'], function (opts) {
-    if (opts.JENKINS_USERNAME)
-      $('#jenkinsBuildResults').append('<button id="jenkinsBuild" type="submit" style="float: right" class="button primary">Build</button><br/><br/>');
-
-    $('#jenkinsBuild').click(function () {
-      $.post(URL + '?' + $.param(opts), function () { location.reload(); });
-    });
+      });
+    }
   });
-});
 }
+
+function pullRequest(pr, opts) {
+  var url = 'http://' + HOSTNAME + pr;
+  $.getJSON(url, function (data) {
+    $('#pr_contributors_box').before('<div id="jenkins"></div>');
+    $('#jenkins').append('<div id="jenkinsBuildIndicator" ' + getSI(data, '') + '>'+ data.status + '</div>');
+    $('#jenkins').append('<div id="jenkinsBuildResults" class="discussion-bubble-inner"></div>');
+
+    if (data.status != 'PASSING') {
+      var html = '<table class="commits commits-conendsed" width="100%">';
+      data.result = data.result || {};
+      $.each(data.result, function (i) {
+        var result = data.result[i];
+        if (!result) return;
+        if (result.length) {
+          result.forEach(function (test) {
+            html += '<tr class="commit js-details-container js-socket-channel js-updatable-content">';
+            html += '<td class="author">' + i + '</td>';
+            html += '<td class="message"><code>';
+            html += '<a target="_new" href="' + test.url.replace('/artifact/test.tap', '');
+            html += '/tapTestReport/test.tap-' + test.number + '/">';
+            html += test.desc + '</a></code></td>';
+            html += '</tr>';
+          });
+        }
+      });
+      html += '</table>';
+      $('#jenkinsBuildResults').append(html);
+    }
+
+    if (data.url)
+      $('#jenkinsBuildResults').append('<a target="_new" href="' + data.url + '">Last build</a>');
+
+    //if (data.started)
+    //  $('#jenkinsBuildResults').append(' Started at: ' + data.started);
+
+    if (data.by)
+      $('#jenkinsBuildResults').append(' By: ' + data.by);
+
+    if (canBuild(opts) && data.status != 'BUILDING') {
+      var html = '<button style="float: right" class="button primary jenkins-build" data-pr="';
+      html += window.location.pathname+'">Build</button><br/><br/>';
+
+      $('#jenkinsBuildResults').append(html);
+
+      $('#jenkinsBuild').click(function () {
+        buildPR(this, opts);
+      });
+    }
+  });
+}
+
+chrome.storage.sync.get(STORAGE, function (opts) {
+  if (window.location.pathname.match(/\d+$/)) {
+    pullRequest(window.location.pathname, opts);
+  } else {
+    pullList(opts);
+  }
+});
